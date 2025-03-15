859c62c7be7cb12c7e82221fb6be8c4a9b075d99
[CLOUD4] 한현 <gusgus1467@naver.com>
2025-03-15 21:06:44 +0900

    init

---
+++ src/main/java/com/news_miner/NewsMinerApplication.java
@@ -0,0 +1,13 @@
+package com.news_miner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsMinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/news_miner/NewsMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news_miner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsMinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
