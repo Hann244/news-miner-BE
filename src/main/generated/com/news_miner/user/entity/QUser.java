97f85a9f3603abd498d67107cd841b79dafdde0f
[CLOUD4] 한현 <gusgus1467@naver.com>
2025-03-21 00:11:26 +0900

    feat : 기본 설정 추가

---
+++ src/main/generated/com/news_miner/user/entity/QUser.java
@@ -0,0 +1,59 @@
+package com.news_miner.user.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = 1630187777L;
+
+    public static final QUser user = new QUser("user");
+
+    public final com.news_miner.global.entity.QBaseTimeEntity _super = new com.news_miner.global.entity.QBaseTimeEntity(this);
+
+    //inherited
+    public final DateTimePath<java.sql.Timestamp> createdAt = _super.createdAt;
+
+    public final DateTimePath<java.time.LocalDateTime> deletedAt = createDateTime("deletedAt", java.time.LocalDateTime.class);
+
+    public final StringPath deleteReason = createString("deleteReason");
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath password = createString("password");
+
+    public final EnumPath<com.news_miner.user.enums.Role> role = createEnum("role", com.news_miner.user.enums.Role.class);
+
+    //inherited
+    public final DateTimePath<java.sql.Timestamp> updatedAt = _super.updatedAt;
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
